0e906e723dfc47e56c783ae09a040f941577c019
briantkho <briantkho@gmail.com>
2023-02-02 00:23:35 -0800

    Renaming

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Capstone</title>
+      <title>OneTwo3</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Your new you starts here" />
       <link rel="icon" href="/favicon.ico" />
